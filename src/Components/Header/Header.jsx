472cb97b2fb21bbed3d2a0b9232e406d3792c0e1
Amsav23 <130859471+Amsav23@users.noreply.github.com>
2023-12-12 13:15:31 -0500

    Header.jsx
    Created the header component. UniLife home link works.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Header.css'
+import unlifeHouseLogo from '../../assets/unlifeHouseLogo.png'
+import { IoIosHeartEmpty } from "react-icons/io";
+import { CiMail } from "react-icons/ci";
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header-container'>
+      <Link to='/' className='uniLife_logo'>
+        <img src={unlifeHouseLogo} alt="UniLife Logo" />
+        <h1>UniLife</h1>
+      </Link>
+
+      <div className='navBar-container'>
+        <p className='navBar-icons'> <IoIosHeartEmpty /> Shortlist</p>
+        <p className='navBar-icons'> <CiMail /> Contact Us</p>
+      </div>
+      
+      
+      
+    </div>
   )
 }
 
